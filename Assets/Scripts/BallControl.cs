8959f49d4439e949aa49fc9961c02ee5888a3a40
Cemre <110540754+Rainy-Mask@users.noreply.github.com>
2023-08-27 12:05:30 +0300

    Rotation 1

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -7,12 +7,20 @@ public class BallControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       //transform.Translate(0,0,10); 
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { 
+        //transform.Translate(0,0,10); 
+        //transform.Rotate(0,0,-0.1f);
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("ESC tuşuna basıldı!");
+        }
         
+            
+
     }
 }
